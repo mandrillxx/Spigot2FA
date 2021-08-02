aeba085cf52a3c80a3f112112ed3e350500278b4
sdfaken <sdfaken@users.noreply.github.com>
2021-08-01 21:27:36 -0400

    Initial Commit

---
+++ src/main/java/me/mandrillxx/spigot2fa/Spigot2FA.java
@@ -0,0 +1,18 @@
+package me.mandrillxx.spigot2fa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Spigot2FA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
